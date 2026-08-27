42cc1e11b1d7ae65e50227d9d5a4ce1057714387
leguissamon <leguissamonk@gmail.com>
2026-08-27 17:21:09 -0400

    codigo base

---
+++ aula04/src/App.jsx
@@ -0,0 +1,10 @@
+import ListaUsuarios from "./components/ListaUsuarios";
+
+export default function App(){
+  return(
+    <section>
+    <h1>Lista de Usuarios</h1>
+    <ListaUsuarios/>
+    </section>
+  )
+}
