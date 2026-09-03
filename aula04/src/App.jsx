9c83bbd535c4986dec00198a28451667f0f21cbc
leguissamon <leguissamonk@gmail.com>
2026-09-03 17:05:38 -0400

    exercicios1,2,3,4,5 em componentes e comentados

--- aula04/src/App.jsx
+++ aula04/src/App.jsx
@@ -7,6 +7,8 @@ export default function App(){
     <>
       <section>
         <h1>Exercícios 1, 2, 3 e 4</h1>
+        <p>Professor, eu deixei o exercicio 2 e 4 em forma de um fetch comentado, basta ir no codigo, comentar o primeiro fetch e descomentar o segundo
+          fetch para testar</p>
         <ListaUsuarios/>
       </section>
 
